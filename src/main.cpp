0571d9b8d1f462140aee15ebe476620691fb03f9
Yeonjun Lee <yeonfish6040@gmail.com>
2024-10-31 20:27:38 +0900

    interval, bulb interval

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <LiquidCrystal_I2C.h>
 
 #define out DD2
+#define breakOnEnter if (Serial.read() == '\n') break;
 
 LiquidCrystal_I2C lcd(0x3f, 16, 2);
 
@@ -14,18 +15,19 @@ void setup() {
 
     lcd.init();
     lcd.backlight();
+    lcd.clear();
+    lcd.home();
 }
 
 
 // mode 0: bulb
 // mode 1: interval
-// mode 2: bulb interval - bulb
-// mode 3: bulb interval - interval
+// mode 2: bulb interval
 int mode = 0;
 
-int bulb = 20;
-int intervalDuration = 1000;
-int intervalRepeat = 3;
+unsigned int bulb = 1;
+unsigned int intervalDuration = 30;
+unsigned int intervalRepeat = 3;
 
 bool repaint = true;
 bool running = false;
@@ -46,19 +48,19 @@ void loop() {
                 lcd.print("Interval mode");
                 lcd.setCursor(0, 1);
                 lcd.print(intervalDuration);
-                lcd.print("ms ");
+                lcd.print("00ms ");
                 lcd.print(intervalRepeat);
                 lcd.print("ea");
                 break;
             case 2:
             case 3:
                 lcd.setCursor(0, 0);
-                lcd.print(mode == 2 ? "Both mode - b" : "Both mode - i");
+                lcd.print("Both mode");
                 lcd.setCursor(0, 1);
                 lcd.print(bulb);
                 lcd.print("s ");
                 lcd.print(intervalDuration);
-                lcd.print("ms ");
+                lcd.print("s ");
                 lcd.print(intervalRepeat);
                 lcd.print("ea");
                 break;
@@ -78,12 +80,81 @@ void loop() {
 
             digitalWrite(out, HIGH);
             unsigned long time = millis();
-            while (millis()-time < bulb*1000) {
+            while (millis()-time < (bulb*1000)) {
                 lcd.setCursor(0, 1);
                 lcd.print(bulb-(millis()-time)/1000);
                 lcd.print("s  ");
+
+                breakOnEnter
             }
             digitalWrite(out, LOW);
+
+            lcd.clear();
+            lcd.home();
+            lcd.setCursor(0, 0);
+            lcd.print("Complete!");
+            delay(2000);
+        }
+        if (mode == 1) {
+            lcd.clear();
+            lcd.home();
+            lcd.setCursor(0, 0);
+            lcd.print("Running... Interval");
+
+            int repeatTmp = intervalRepeat;
+            unsigned long time = millis();
+            while (repeatTmp != 0) {
+                while ((millis()-time)/(intervalDuration*100) == (intervalRepeat-repeatTmp)) {
+                    lcd.setCursor(0, 1);
+                    lcd.print(((intervalDuration*100)-((millis()-time)%(intervalDuration*100)))/100);
+                    lcd.print("00ms ");
+                    lcd.print(repeatTmp);
+                    lcd.print("ea   ");
+
+                    breakOnEnter
+                }
+                digitalWrite(out, HIGH);
+                delay(10);
+                digitalWrite(out, LOW);
+                repeatTmp--;
+            }
+
+            lcd.clear();
+            lcd.home();
+            lcd.setCursor(0, 0);
+            lcd.print("Complete!");
+            delay(2000);
+        }
+        if (mode == 2) {
+            lcd.clear();
+            lcd.home();
+            lcd.setCursor(0, 0);
+            lcd.print("Running... both");
+
+            int repeatTmp = intervalRepeat;
+            unsigned long time = millis();
+            while (repeatTmp != 0) {
+                while ((millis()-time)/(intervalDuration*1000) == (intervalRepeat-repeatTmp)) {
+                    lcd.setCursor(0, 1);
+                    lcd.print(((intervalDuration*1000)-((millis()-time)%(intervalDuration*1000)))/1000);
+                    lcd.print("s ");
+                    lcd.print(repeatTmp);
+                    lcd.print("ea   ");
+
+                    breakOnEnter
+                }
+                digitalWrite(out, HIGH);
+                unsigned long time2 = millis();
+                while (millis()-time2 < bulb*1000) {
+                    lcd.setCursor(0, 1);
+                    lcd.print(bulb-(millis()-time2)/1000);
+                    lcd.print("s               ");
+
+                    breakOnEnter
+                }
+                digitalWrite(out, LOW);
+                repeatTmp--;
+            }
 
             lcd.clear();
             lcd.home();
@@ -102,8 +173,20 @@ void loop() {
                     read = Serial.read();
                 } while (read == -1);
                 mode = read-'0';
-                if (mode > 3) mode = mode % 3;
+                if (mode > 2) mode = mode % 3;
             }
+            if (read == 'q') bulb++;
+            if (read == 'w') intervalDuration++;
+            if (read == 'e') intervalRepeat++;
+            if (read == 'a') bulb--;
+            if (read == 's') intervalDuration--;
+            if (read == 'd') intervalRepeat--;
+            if (read == 'r') bulb += 10;
+            if (read == 't') intervalDuration += 10;
+            if (read == 'y') intervalRepeat += 10;
+            if (read == 'f') bulb -= 10;
+            if (read == 'g') intervalDuration -= 10;
+            if (read == 'h') intervalRepeat -= 10;
             if (read == '\n') running = true;
             repaint = true;
         }
